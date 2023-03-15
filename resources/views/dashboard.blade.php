12bea507529478130ba2559a14910e96f6d37963
JOSÉ DIOGO TEIXEIRA DE MAGALHÃES <8210125@estg.ipp.pt>
2023-03-15 15:44:16 +0000

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,42 +4,60 @@
             {{ __('Eventos') }}
         </h2>
     </x-slot>
-    <head>
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    </head>
-        <h1 class="text-center my-4" style="font-size: 24px; margin-top: 20px;">Lista de Eventos </h1>
-        <br />
-        <table style="margin: 20px auto 0;">
-        <thead>
-            <style>
-                table, th, td {
-                    border: 1px solid black;
-                    border-collapse: collapse;
-                    padding: 10px;
-                    text-align: center;
-                }
-            </style>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Localização</th>
-                <th>Data e Hora</th>
-                <th>Número de Vagas</th>
-                <th>Vagas Disponíveis</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($events as $event)
-                <tr>
-                    <td>{{ $event->id }}</td>
-                    <td>{{ $event->nome }}</td>
-                    <td>{{ $event->localizacao }}</td>
-                    <td>{{ $event->data_hora }}</td>
-                    <td>{{ $event->numero_vagas }}</td>
-                    <td>{{ $event->vagas_disponiveis }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    
-    </x-app-layout>
+
+    <div class="eventos-container">
+        @foreach ($events as $event)
+            <div class="evento">
+                <h1 class="text-center my-4" style="font-size: 24px; margin-top: 20px;">{{ $event->nome }}</h1>
+                <div class="evento-info">
+                    <div class="evento-localizacao">
+                        <strong>Localização:</strong> {{ $event->localizacao }}
+                    </div>
+                    <div class="evento-data">
+                        <strong>Data e Hora:</strong> {{ $event->data_hora }}
+                    </div>
+                    <div class="evento-vagas">
+                        <strong>Número de Vagas:</strong> {{ $event->numero_vagas }}
+                    </div>
+                    <div class="evento-disponiveis">
+                        <strong>Vagas Disponíveis:</strong> {{ $event->vagas_disponiveis }}
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+</x-app-layout>
+
+<style>
+.eventos-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.evento {
+    background-color: #f8f8f8;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    margin: 20px;
+    padding: 20px;
+    max-width: 600px;
+    flex: 1;
+    flex-basis: calc(50% - 20px);
+}
+.evento h1 {
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+.evento-info {
+    margin-top: 20px;
+}
+.evento-info div {
+    margin-bottom: 10px;
+}
+.evento-info strong {
+    display: inline-block;
+    min-width: 150px;
+    font-weight: bold;
+}
+</style>
