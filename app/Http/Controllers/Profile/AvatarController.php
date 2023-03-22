e7eb322e80fd0448c574a340e7534ab832d43a97
JOSÉ DIOGO TEIXEIRA DE MAGALHÃES <8210125@estg.ipp.pt>
2023-03-22 17:20:05 +0000

    update

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request)
     {
-        // auth()->user()->update(['avatar' => 'test']);
-        // dd(auth()->user());
-        $path = $request->file('avatar')->store('avatars');
+        
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+        // $path = $request->file('avatar')->store('avatars', 'public');
+
+        if($oldAvatar = $request->user()->avatar){
+            Storage::disk('public')->delete($oldAvatar);
+        }
+
+        auth()->user()->update(['avatar' => $path]);
 
         return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
         
